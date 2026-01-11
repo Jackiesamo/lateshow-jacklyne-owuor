4fe9bf5e598abc0f601e82aa27d5143474ef2678
jacklyne <jacklyne.owuor@student.moringaschool>
2026-01-11 21:37:19 +0300

    added class episode

--- models.py
+++ models.py
@@ -2,3 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+class Episode(db.Model):
+    __tablename__ = 'episodes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String)
+    number = db.Column(db.Integer)
+
+    appearances = db.relationship(
+        'Appearance',
+        back_populates='episode',
+        cascade='all, delete-orphan'
+    )
