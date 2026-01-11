be99d0573f2756e206100f936ff6fd95b99dfee6
jacklyne <jacklyne.owuor@student.moringaschool>
2026-01-11 21:58:40 +0300

    added route get episode by id

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ app = create_app()
 def get_episodes():
     episodes = Episode.query.all()
     return jsonify([e.to_dict() for e in episodes]), 200
+
+@app.route('/episodes/<int:id>', methods=['GET'])
+def get_episode_by_id(id):
+    episode = Episode.query.get(id)
+
