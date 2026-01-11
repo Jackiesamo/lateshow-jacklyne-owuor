9a855c267ebfbce94656ae52148a9f6edeed37d6
jacklyne <jacklyne.owuor@student.moringaschool>
2026-01-11 22:05:26 +0300

    updated app.py

--- app.py
+++ app.py
@@ -37,3 +37,34 @@ def get_guests():
     guests = Guest.query.all()
     return jsonify([g.to_dict() for g in guests]), 200
 
+
+@app.route('/appearances', methods=['POST'])
+def create_appearance():
+    try:
+        data = request.get_json()
+
+        appearance = Appearance(
+            rating=data['rating'],
+            episode_id=data['episode_id'],
+            guest_id=data['guest_id']
+        )
+
+        db.session.add(appearance)
+        db.session.commit()
+
+        return jsonify({
+            "id": appearance.id,
+            "rating": appearance.rating,
+            "episode_id": appearance.episode_id,
+            "guest_id": appearance.guest_id,
+            "episode": appearance.episode.to_dict(),
+            "guest": appearance.guest.to_dict()
+        }), 201
+
+    except Exception as e:
+        return jsonify({"errors": [str(e)]}), 400
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
