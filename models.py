3ab44e3877afaf5588202510647b28db65615f9e
jacklyne <jacklyne.owuor@student.moringaschool>
2026-01-11 21:40:51 +0300

    added class guest

--- models.py
+++ models.py
@@ -22,3 +22,16 @@ class Episode(db.Model):
             "date": self.date,
             "number": self.number
         }
+    
+    class Guest(db.Model):
+     __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    occupation = db.Column(db.String)
+
+    appearances = db.relationship(
+        'Appearance',
+        back_populates='guest',
+        cascade='all, delete-orphan'
+    )
