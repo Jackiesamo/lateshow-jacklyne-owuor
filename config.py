6fa5b57978ecf830785acffc3f32f623cad72530
jacklyne <jacklyne.owuor@student.moringaschool>
2026-01-11 21:52:05 +0300

    updated config

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
 from flask import Flask
 from flask_migrate import Migrate
 from models import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lateshow.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
