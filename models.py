98bdd27b18abb757174116a60c3b55a572843d0c
jacklyne <jacklyne.owuor@student.moringaschool>
2026-01-11 21:47:21 +0300

    added validation in class apperance

--- models.py
+++ models.py
@@ -55,4 +55,10 @@ class Appearance(db.Model):
     episode = db.relationship('Episode', back_populates='appearances')
     guest = db.relationship('Guest', back_populates='appearances')
 
+    @validates('rating')
+    def validate_rating(self, key, rating):
+        if rating < 1 or rating > 5:
+            raise ValueError("Rating must be between 1 and 5")
+        return rating
+
     
